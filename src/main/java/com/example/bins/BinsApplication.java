960a03f4c930584b2010e608c9c25205d146bf90
MunChangbin <munchangbin@munchangbin-ui-MacBookAir.local>
2023-07-07 09:34:46 +0900

    test

---
+++ src/main/java/com/example/bins/BinsApplication.java
@@ -0,0 +1,13 @@
+package com.example.bins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bins/BinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
